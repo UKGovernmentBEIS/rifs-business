506b627cac2957e3f487d5516cf4d0566f3db6df
Doug Clinton <doug@wiredthing.com>
2016-10-05 15:59:41 +0100

    Created compiled queries for ApplicationTables

--- src/main/scala/rifs/business/tables/ApplicationTables.scala
+++ src/main/scala/rifs/business/tables/ApplicationTables.scala
@@ -20,21 +20,28 @@ class ApplicationTables @Inject()(dbConfigProvider: DatabaseConfigProvider)(impl
 
   import driver.api._
 
-  override def byId(id: ApplicationId): Future[Option[ApplicationRow]] = db.run {
-    applicationTable.filter(_.id === id).result.headOption
-  }
+  override def byId(id: ApplicationId): Future[Option[ApplicationRow]] = db.run(byIdC(id).result.headOption)
 
   override def forOpportunity(opportunityId: OpportunityId): Future[Option[Application]] = db.run {
-    val q = for {
-      a <- applicationTable if a.opportunityId === opportunityId
-      s <- applicationSectionTable if s.applicationId === a.id
-    } yield (a, s)
-
-    q.result.map {
-      rs =>
-        rs.groupBy(_._1).map { case (k, vs) => k -> vs.map(_._2) }.map {
-          case (a, ss) => Application(a.id, a.opportunityId, ss.map(s => ApplicationSection(s.sectionNumber, s.title, s.started)))
-        }
+    applicationByOppIdC(opportunityId).result.map { rs =>
+      rs.groupBy(_._1).map { case (k, vs) => k -> vs.map(_._2) }.map {
+        case (a, ss) => Application(a.id, a.opportunityId, ss.map(s => ApplicationSection(s.sectionNumber, s.title, s.started)))
+      }
     }.map(_.headOption)
   }
+
+  /*
+  ******************************
+  * Queries and compiled queries
+   */
+  def byIdQ(id: Rep[ApplicationId]) = for {a <- applicationTable if a.id == id} yield a
+
+  val byIdC = Compiled(byIdQ _)
+
+  def applicationByOppIdQ(opportunityId: Rep[OpportunityId]) = for {
+    a <- applicationTable if a.opportunityId === opportunityId
+    s <- applicationSectionTable if s.applicationId === a.id
+  } yield (a, s)
+
+  val applicationByOppIdC = Compiled(applicationByOppIdQ _)
 }
