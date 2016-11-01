b82600117dd717eadfb6473d8a80b3cf0fd51932
Doug Clinton <doug@wiredthing.com>
2016-11-01 16:40:33 +0000

    Added endpoints to create and update items in a section

--- src/main/scala/rifs/business/controllers/ApplicationController.scala
+++ src/main/scala/rifs/business/controllers/ApplicationController.scala
@@ -4,12 +4,13 @@ import javax.inject.Inject
 
 import org.joda.time.LocalDateTime
 import play.api.cache.Cached
-import play.api.libs.json.JsObject
+import play.api.libs.json._
 import play.api.mvc.{Action, Controller}
 import rifs.business.data.ApplicationOps
 import rifs.business.models.{ApplicationFormId, ApplicationId}
 
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ExecutionContext, Future}
+import scala.util.Random
 
 class ApplicationController @Inject()(val cached: Cached, applications: ApplicationOps)(implicit val ec: ExecutionContext) extends Controller with ControllerUtils {
   def byId(id: ApplicationId) = cacheOk {
@@ -25,7 +26,7 @@ class ApplicationController @Inject()(val cached: Cached, applications: Applicat
     * If the `id` does not match an existing `ApplicationForm` then return a 404
     */
   def overview(applicationId: ApplicationId) =
-    Action.async(applications.overview(applicationId).map(jsonResult(_)))
+  Action.async(applications.overview(applicationId).map(jsonResult(_)))
 
   def section(id: ApplicationId, sectionNumber: Int) =
     Action.async(applications.fetchSection(id, sectionNumber).map(jsonResult(_)))
@@ -34,7 +35,51 @@ class ApplicationController @Inject()(val cached: Cached, applications: Applicat
     applications.saveSection(id, sectionNumber, request.body).map(_ => NoContent)
   }
 
-  def completeSection(id: ApplicationId, sectionNumber: Int) = Action.async(parse.json[JsObject]) { implicit request =>
-    applications.saveSection(id, sectionNumber, request.body, Some(LocalDateTime.now())).map(_ => NoContent)
+  def updateSectionItem(id: ApplicationId, sectionNumber: Int, itemNumber: Int) = Action.async(parse.json[JsObject]) { implicit request =>
+
+    def hasItemNumber(o: JsObject, num: Int) = o \ "itemNumber" match {
+      case JsDefined(JsNumber(n)) if n == num => true
+      case _ => false
+    }
+
+    applications.fetchSection(id, sectionNumber).flatMap { os =>
+      val doc = os.map(_.answers).getOrElse(JsObject(Seq()))
+      val items = doc \ "items" match {
+        case JsDefined(JsArray(is)) => is.collect { case o: JsObject => o }
+        case _ => Seq()
+      }
+      val remainingItems = items.filterNot(o => hasItemNumber(o, itemNumber))
+      val newItem = request.body + ("itemNumber" -> JsNumber(itemNumber))
+      val updated = doc + ("items" -> JsArray(newItem +: items))
+      applications.saveSection(id, sectionNumber, updated).map(_ => NoContent)
+    }
+  }
+
+  def createSectionItem(id: ApplicationId, sectionNumber: Int) = Action.async(parse.json[JsObject]) { implicit request =>
+    applications.fetchSection(id, sectionNumber).flatMap { os =>
+      val doc = os.map(_.answers).getOrElse(JsObject(Seq()))
+
+      val items: Seq[JsValue] = doc \ "items" match {
+        case JsDefined(JsArray(is)) => is
+        case _ => Seq()
+      }
+
+      allocateItemNumber().flatMap { n =>
+        val newItem = request.body + ("itemNumber" -> JsNumber(n))
+        val updated = doc + ("items" -> JsArray(newItem +: items))
+
+        applications.saveSection(id, sectionNumber, updated)
+      }
+    }.map(_ => NoContent)
+  }
+
+  def allocateItemNumber(): Future[Int] = Future {
+    Random.nextInt().abs
+  }
+
+
+  def completeSection(id: ApplicationId, sectionNumber: Int) = Action.async(parse.json[JsObject]) {
+    implicit request =>
+      applications.saveSection(id, sectionNumber, request.body, Some(LocalDateTime.now())).map(_ => NoContent)
   }
 }
