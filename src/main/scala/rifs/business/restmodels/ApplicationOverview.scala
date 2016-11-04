940e85e6d85ebfc1768e7db8ea0be2f23cb2db8f
BenKDishman <ben.dishman@digital.bis.gov.uk>
2016-11-04 15:27:09 +0000

    Final Commit

--- src/main/scala/rifs/business/restmodels/ApplicationOverview.scala
+++ src/main/scala/rifs/business/restmodels/ApplicationOverview.scala
@@ -4,6 +4,6 @@ import org.joda.time.LocalDateTime
 import play.api.libs.json.JsObject
 import rifs.business.models.{ApplicationFormId, ApplicationId}
 
-case class ApplicationSection(sectionNumber: Int,  answers: JsObject, completedAt: Option[LocalDateTime])
+case class ApplicationSection(sectionNumber: Int, answers: JsObject, completedAt: Option[LocalDateTime])
 
 case class Application(id: ApplicationId, applicationFormId: ApplicationFormId, sections: Seq[ApplicationSection])
