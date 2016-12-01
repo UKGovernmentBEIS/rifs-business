20a4c4139b1b33802c0dd30ace17409c1179a4ed
Doug Clinton <doug@wiredthing.com>
2016-12-01 10:42:32 +0000

    Made a couple of changes to the init-user-db.sh script. Added an explicit `--host=localhost` parameter. By default psql tries to bind to the socket connection but when running in docker the db doesn't create a socket. Also, use the `postgres` user which, in the docker build, is the default user and has privileges to run the script items.

--- docker/postgres/init-user-db.sh
+++ docker/postgres/init-user-db.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+psql -v ON_ERROR_STOP=1 --host=localhost --username postgres <<-EOSQL
     CREATE USER rifs;
     CREATE DATABASE rifs;
     GRANT ALL PRIVILEGES ON DATABASE rifs TO rifs;
