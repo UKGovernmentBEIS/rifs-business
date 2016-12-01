7fa21f7bb6b94d8bf7b6e9f80ed6eaa93c067724
Doug Clinton <doug@wiredthing.com>
2016-12-01 16:47:27 +0000

    Can't json validate an Option[String] so expect a string but if it's blank convert to None

--- src/main/scala/rifs/business/controllers/manage/OpportunityController.scala
+++ src/main/scala/rifs/business/controllers/manage/OpportunityController.scala
@@ -3,7 +3,6 @@ package rifs.business.controllers.manage
 import javax.inject.Inject
 
 import play.api.cache.Cached
-import play.api.libs.json.JsString
 import play.api.mvc.{Action, Controller}
 import rifs.business.controllers.ControllerUtils
 import rifs.business.data.OpportunityOps
@@ -13,8 +12,11 @@ import scala.concurrent.ExecutionContext
 
 class OpportunityController @Inject()(val cached: Cached, opportunities: OpportunityOps)(implicit val ec: ExecutionContext) extends Controller with ControllerUtils {
 
-  def saveDescription(id: OpportunityId, sectionNum: Int) = Action.async(parse.json[Option[JsString]]) { implicit request =>
-    val description = request.body.map(_.value)
+  def saveDescription(id: OpportunityId, sectionNum: Int) = Action.async(parse.json[String]) { implicit request =>
+    val description = request.body.trim match {
+      case "" => None
+      case s => Some(s)
+    }
 
     opportunities.saveSectionDescription(id, sectionNum, description).map {
       case 0 => NotFound
