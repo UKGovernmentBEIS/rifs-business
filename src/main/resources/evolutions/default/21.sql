b9ad27085fcf38103b3f6eef72f2609a6284c3aa
Doug Clinton <doug@wellfactored.com>
2016-12-02 09:49:42 +0000

    Set published date of existing opportunity so that is is considered "active" by the frontend.

--- src/main/resources/evolutions/default/21.sql
+++ src/main/resources/evolutions/default/21.sql
@@ -5,6 +5,8 @@ alter table "opportunity" add column "duplicated_from_id" BIGINT;
 
 alter table "opportunity" add constraint "duplicated_opportunity_fk" foreign key("duplicated_from_id") references "opportunity"("id") on update NO ACTION on delete CASCADE;
 
+update "opportunity" set "published_at_dtime" = '2016-11-28 00:00:00' where id = 1;
+
 # --- !Downs
 
 alter table "opportunity" drop constraint "duplicated_opportunity_fk";
